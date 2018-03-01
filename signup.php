394a25e303b6358b82fbb77da08f1bee4362828a
firasmalik@gmail.com <firasmalik@gmail.com>
2018-03-01 14:03:18 +0530

    interface done

---
+++ signup.php
@@ -0,0 +1,40 @@
+<?php 
+
+
+include_once 'header.php' ;
+?>
+
+<section class='main-container'>
+	
+      <div class='main-wrapper'>
+      	       <h2>Signup</h2>
+                <form class='signup-form'>
+      		
+        <input type="text" name="first" placeholder="Firstname">
+        <br>
+        <input type="text" name="last" placeholder="Lasttname">
+        <br>
+        <input type="text" name="email" placeholder="E-mail">
+        <br>
+        <input type="text" name="uid" placeholder="Username">
+        <br>
+        <input type="text" name="pwd" placeholder="Password">
+        <br>
+        <button type="submit" name='submit'>Sign up</button>
+        <br>
+      	</form>
+
+
+
+
+      </div>
+</section>
+
+<?php
+
+include_once 'footer.php' ;
+
+
+
+
+?>
